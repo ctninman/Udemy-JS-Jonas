2bed73349436c6f2e3774fd362db5820d33662e2
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-08 14:59:10 -0400

    this keyword lesson

--- test-code.js
+++ test-code.js
@@ -1,2 +1,36 @@
-alert("Welcome to the browser")
 console.log('test code activated')
+
+const chris = {
+	firstName: "Chris",
+	lastName: 'Ninman',
+	birthYear: 1984,
+	job: 'software engineer',
+	friends: ['Michael', "Dwight", "Jim"],
+	hasDriversLicense: false,
+	calcAge: function (birthYear) {
+		return 2037 - birthYear
+	},
+
+	thisCalcAge: function () {
+		console.log(this)
+		return 2037 - this.birthYear
+	},
+
+	thisAgeCalc () {
+		this.age = 2037 - this.birthYear;
+		return this.age
+	},
+
+	getSummary: function () {
+		return `${this.firstName} is a ${this.thisCalcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+	}
+}
+
+console.log (chris.lastName)
+console.log(chris.calcAge(1984));
+console.log(chris.thisCalcAge(1984));
+
+
+console.log(chris['calcAge'](1984));
+
+console.log(chris.getSummary());
