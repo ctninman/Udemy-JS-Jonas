c8053f8e8d3889ea0465af277e7a05656043f066
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-09 14:01:24 -0400

    dev skills coding challenge alternate solution

--- developer-skills.js
+++ developer-skills.js
@@ -32,3 +32,15 @@ function printForecast (tempArray) {
 }
 
 printForecast([14,16,18,22,31])
+
+const printTheForecast = function (tempArray) {
+	let str = ''
+	for (let i = 0; i < tempArray.length; i++) {
+		let daysForecast = `...${tempArray[i]} degrees in ${i + 1} ${i === 0 ? 'day' : 'days'} `
+		str += daysForecast
+	}
+	console.log(str);
+}
+
+printTheForecast([2,5,2,6,3])
+printTheForecast([24,19, 21,15,12,16,31])
