902e614cb773883e26bfb2c7de2e892fb54ae9dc
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-12 12:34:03 -0400

    query selector addition

--- guess-my-number.js
+++ guess-my-number.js
@@ -1,1 +1,13 @@
 'use strict'
+
+let doc = document.querySelector('.message')
+
+console.log(doc.textContent);
+
+document.querySelector('.message').textContent = 'Correct Number'
+
+document.querySelector('.number').textContent = 13
+
+document.querySelector('.score').textContent = 10
+
+document.querySelector('.guess').value = 23
