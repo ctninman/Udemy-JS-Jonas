4faf5b8cc426334615ca13e203d140d9f0d3c379
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-23 09:35:17 -0400

    this keyword code

---
+++ this-keyword.js
@@ -0,0 +1,50 @@
+'use strict'
+
+// WINDOW //
+console.log(this)
+
+
+// UNDEFINED when in strict mode //
+// has its own this keyword, but it is undefined
+const calcAge = function (birthYear) {
+	console.log (2022 - birthYear);
+	console.log(this)
+}
+
+calcAge(1984)
+
+
+// WINDOW, arrow functions don't get their own 'this' keyword, so 'this' takes on the parent 'this', in this case 'window'
+const calcAgeArrow = birthYear => {
+	console.log (2022 - birthYear);
+	console.log(this)
+}
+
+calcAgeArrow(1977)
+
+const chris = {
+	year: 1984,
+	// OBJECT THAT CALLS THE METHOD = "THIS"
+	calcAgeMethod: function () {
+		console.log(2022 - this.year);
+		console.log(this);
+	}
+}
+
+chris.calcAgeMethod();
+
+const matteo = {
+	year: 2017
+}
+
+// matteo borrows chris' method, 'this' points to 'matteo' because 'this' points to whatever object calls the method
+matteo.calcAgeMethod = chris.calcAgeMethod
+
+matteo.calcAgeMethod()
+
+const f = chris.calcAgeMethod
+
+console.log(f)
+
+// UNDEFINED because calcAgeMethod is not called by an object, so 'this' is undefined
+f()
